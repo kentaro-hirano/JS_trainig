b6a2917a95e3eae6f2ef1da11e2a16dca63c3fae
kentaro hirano <yakyukentaro@gmail.com>
2021-05-07 17:44:52 +0900

    finish chapter4-6

---
+++ chapter5/app.js
@@ -0,0 +1,28 @@
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+$(function() {
+    $('.box1').slideDown(function() {
+        $('.box1').css({
+            'background-color': '#0000FF',
+            'height': '100px'
+        }).slideUp();
+    });
+});
+
+// $(function() {
+//     $('.box1').slideUp();
+// });
+
+// $(function() {
+//     $('.box1').show();
+//     $('.box1').css({ 'background-color': '#0000FF' });
+// });
+
+// $(function() {
+//     $('.box1').hide();
+// });
